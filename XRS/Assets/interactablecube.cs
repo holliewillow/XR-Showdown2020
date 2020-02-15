2bff8768744db6a14064fd9e2ca719232918a848
holliewillow <46681264+holliewillow@users.noreply.github.com>
2020-02-14 21:02:52 -0600

    cube change color on grab

---
+++ XRS/Assets/interactablecube.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.GrabAttachMechanics;
+using VRTK.SecondaryControllerGrabActions;
+
+
+
+public class interactablecube : MonoBehaviour
+{
+    public VRTK.VRTK_InteractableObject obj;
+    public Material m1;
+    public Material m2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (obj.IsGrabbed())
+        {
+            gameObject.GetComponent<Renderer>().material = m2;
+            Debug.Log("You grabbed me!");
+        }
+        else if (!obj.IsGrabbed())
+        {
+            gameObject.GetComponent<Renderer>().material = m1;
+        }
+    }
+}
